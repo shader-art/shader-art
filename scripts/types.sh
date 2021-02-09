d56ea68006dcd93927664b58f3ff1a4035c1e321
Lea Rosema <terabaud@gmail.com>
2021-02-09 13:36:47 +0100

    chore: try to fix typings

--- scripts/types.sh
+++ scripts/types.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-tsc -t esnext --moduleResolution node -d --emitDeclarationOnly --outFile dist/shader-art.d.ts src/shader-art.ts
-tsc -t esnext --moduleResolution node -d --emitDeclarationOnly --outFile dist/plugins/texture-plugin.d.ts src/plugins/texture-plugin.ts
+npx tsc -t esnext --moduleResolution node -d --emitDeclarationOnly --outFile dist/shader-art.d.ts src/shader-art.ts
+npx tsc -t esnext --moduleResolution node -d --emitDeclarationOnly --outFile dist/plugins/texture-plugin.d.ts src/plugins/texture-plugin.ts
