20a39736fa4a936cd6b69c4f825ad340b02ed191
Lea Rosema <terabaud@gmail.com>
2021-02-09 14:29:40 +0100

    fix: make images work

--- src/utils/image-loader.ts
+++ src/utils/image-loader.ts
@@ -1,6 +1,7 @@
 export function loadImage(url: string) {
   return new Promise<HTMLImageElement>((resolve, reject) => {
     const img = new Image();
+    img.crossOrigin = 'anonymous';
     img.src = url;
     img.onload = () => {
       resolve(img);
